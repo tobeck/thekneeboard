166d620dc3cfdff0c9c91287df2da846a10d0001
tobeck <tobias.becker.olsson@gmail.com>
2022-06-20 21:45:04 +0200

    Setting up tRPC test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,17 @@ import type { NextPage } from 'next'
 import { signIn, useSession } from 'next-auth/react'
 import Head from 'next/head'
 import { prisma } from '../db/client'
+import { trpc } from '../utils/trpc' 
 
 const Home: NextPage = (props: any) => {
   const { data: session, status } = useSession()
+  const hello = trpc.useQuery(['hello', { text: 'client'}])
+
+  if (!hello.data) {
+    return (
+      <div>Loading...</div>
+    )
+  }
 
   return (
     <div>
@@ -20,6 +28,9 @@ const Home: NextPage = (props: any) => {
       {session?.user && (
         <>
           <h4>You are signed in as: {session.user.name}</h4>
+          <div>
+            <p>{hello.data.greeting}</p>
+          </div>
         </>
       )}
     </div>
